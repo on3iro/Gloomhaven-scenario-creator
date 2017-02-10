1407b763582bee7e00dca3097b99422ef33fec34
Theo Salzmann <dev@on3iro.de>
2017-02-10 17:21:08 +0100

    Added CI and dot files

---
+++ src/components/app/index.js
@@ -0,0 +1,24 @@
+import React, { Component, PropTypes } from 'react';
+
+import Navigation from '../navigation';
+
+
+export default class App extends Component {
+  /**
+   * Main App component
+   * Wraps children
+    */
+
+  render() {
+    return (
+      <div>
+        <Navigation />
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.element,
+};
