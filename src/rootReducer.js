8dfc2f05fd842983d46e20ed96ffa1576b70163e
Theo Salzmann <dev@on3iro.de>
2017-03-11 09:55:43 +0100

    Removed routerReducer for now, because react-router-redux does not work

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
 import { reducer as formReducer } from 'redux-form';
 
 import auth from 'containers/Auth/ducks/reducers';
 
 
 export default combineReducers({
-  routing: routerReducer,
   form: formReducer,
   auth,
 });
