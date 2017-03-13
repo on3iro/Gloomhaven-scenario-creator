fe9a5882f2a678aeecacf7ddfd57f328fcfa3c18
Theo Salzmann <dev@on3iro.de>
2017-03-13 08:41:27 +0100

    Basic redirect to refferer now works

--- src/containers/Auth/index.js
+++ src/containers/Auth/index.js
@@ -5,24 +5,37 @@
   */
 
 import React, { PropTypes, Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
+import * as selectors from './ducks/selectors';
 import Login from './Login';
 
 
 class Auth extends Component {
-  state = {
-    redirectToReferer: false,
-  }
-
   render() {
+    const { from } = this.props.location.state || { from: { pathname: '/' } };
+
+    if(this.props.isLoggedIn) {
+      return (
+        <Redirect to={from} />
+      );
+    }
+
     return (
       <Login />
     );
   }
 }
 
+const mapStateToProps = (state, ownProps) => {
+  return {
+    isLoggedIn: selectors.getLoggedIn(state),
+  };
+};
+
 Auth.propTypes = {
   // TODO
 };
 
-export default Auth;
+export default connect(mapStateToProps, null)(Auth);
