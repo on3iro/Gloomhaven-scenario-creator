e254366803e1db057559c70fc79cef34cd90ba54
Theo Salzmann <dev@on3iro.de>
2017-03-14 13:09:45 +0100

    Fixed broken import in header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,7 @@
 import  React from 'react';
 
 import Navigation from 'components/Navigation';
-import UserNav from 'components/Auth/UserNav';
+import UserNav from 'components/UserNav';
 import A from './A';
 import Wrapper from './Wrapper';
 
