8fa8ae9644e6f32d571794062cae98143c1d395a
Theo Salzmann <dev@on3iro.de>
2017-03-15 10:54:29 +0100

    Fixed a few more tests

--- src/components/StyledLink/tests/index.test.js
+++ src/components/StyledLink/tests/index.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 
 import StyledLink from '../index';
 
 
 describe('<StyledLink />', () => {
   it('should render <Link />', () => {
-    const wrapper = shallow(<StyledLink />);
-    expect(wrapper.find(Link).length).toBe(1);
+    const wrapper = shallow(<StyledLink to="/" />);
+    expect(wrapper.find(Link).length).toMatchSnapshot();
   });
 });
