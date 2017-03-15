6a172f63bcb10e6fe099b92baaf8816cea814e1f
Theo Salzmann <dev@on3iro.de>
2017-03-15 12:20:45 +0100

    Added a few more tests

--- src/containers/Auth/tests/index.test.js
+++ src/containers/Auth/tests/index.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { Redirect } from 'react-router-dom';
 
 import { Auth } from '../index';
 import Login from '../Login';
@@ -26,5 +27,10 @@ describe('<Auth />', () => {
   it('should render <Login />', () => {
     const { renderedWrapper } = renderWrapper();
     expect(renderedWrapper.find(Login).length).toEqual(1);
+  });
+
+  it('should render <Redirect /> when user is logged in', () => {
+    const { renderedWrapper } = renderWrapper({isLoggedIn: true});
+    expect(renderedWrapper.find(Redirect).length).toBe(1);
   });
 });
